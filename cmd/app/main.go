88a4508f89ab52c053c2afaa64c2499866421df5
suxa56 <suxa5600@gmail.com>
2025-05-07 13:56:49 +0500

    add logger

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,16 +8,24 @@ import (
 	"foveo/internal/service"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
+	log := setupLogger(cfg.Env)
+
 	db, err := sqlx.Open("postgres", cfg.PostgresDSN)
 	if err != nil {
-		log.Fatalln(err)
+		log.Error("db error", slog.String("error", err.Error()))
 	}
 	defer db.Close()
 
@@ -31,7 +39,21 @@ func main() {
 	userHandler := handler.InitUserHandler(userService)
 	handler := handler.NewHandler(userHandler)
 
-	log.Println("Server started on :8090")
+	log.Info("Server started on :8090")
 	http.ListenAndServe(":8090", router.NewRouter(handler))
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelWarn}))
+	}
+	return log
 }
